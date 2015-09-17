918b4a27e3ede5c1d09b8688b29f229a51f1babb
albert walker <alwalker57@gmail.com>
2015-09-17 10:12:22 -0400

    fixed timezone and updated admin to include applications and urls

--- polls/admin.py
+++ polls/admin.py
@@ -20,6 +20,8 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Applications)
+admin.site.register(Urls)
 
 
 
